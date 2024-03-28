3f19ef4872012d3e64669fed174e62eaf5f1e5fd
Servan42 <charlot.servan@gmail.com>
2024-03-29 00:55:52 +0100

    feat: First TI on console

--- BankingService.Console/Program.cs
+++ BankingService.Console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BankingService.Core.API.Interfaces;
+using BankingService.Core.Services;
+using BankingService.Infra.Database.Services;
+using BankingService.Infra.FileSystem.Adapters;
+using BankingService.Infra.FileSystem.Services;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var fileSystemService = new FileSystemService();
+        var fileSystemServiceCore = new FileSystemAdapterCore(fileSystemService);
+        var fileSystemServiceDatabase = new FileSystemAdapterDatabase(fileSystemService);
+        var bankDataBaseService = new BankDatabaseService(fileSystemServiceDatabase);
+        IImportService importService = new ImportService(fileSystemServiceCore, bankDataBaseService);
+
+        importService.ImportBankFile(@"F:\Servan\Autres\Code\C#\BankCSVParser\publish\CSV\TODO\***REMOVED***_Jan.csv");
+        importService.ImportBankFile(@"F:\Servan\Autres\Code\C#\BankCSVParser\publish\CSV\TODO\***REMOVED***_Mar.csv");
+    }
+}

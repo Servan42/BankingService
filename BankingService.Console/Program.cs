8a57b3d301ca5c77eab61bdca89e60e0f377701a
Servan42 <charlot.servan@gmail.com>
2024-04-09 00:35:02 +0200

    wip: ask user input

--- BankingService.Console/Program.cs
+++ BankingService.Console/Program.cs
@@ -20,5 +20,33 @@ internal class Program
         importService.RecomputeEveryOperationAdditionalData();
         importService.ImportBankFile(@"F:\Servan\Autres\Code\C#\BankCSVParser\publish\CSV\TODO\***REMOVED***_Jan.csv");
         importService.ImportPaypalFile(@"F:\Servan\Autres\Code\C#\BankCSVParser\publish\CSV\TODO\paypa_janvier.CSV");
+
+        var consoleCategories = new Dictionary<string, string>();
+        int i = 1;
+        foreach(var cat in bankDataBaseService.GetOperationCategoriesAndAutoComment().Values.Select(cat => cat.Category).Distinct()) 
+        {
+            consoleCategories.Add(i.ToString(), cat);
+            i++;
+        }
+
+        foreach(var operationToFill in bankDataBaseService.GetOperationsThatNeedsManualInput())
+        {
+            Console.WriteLine("Categories:");
+            foreach (var cat in consoleCategories) Console.WriteLine($"- [{cat.Key,2}]: {cat.Value}");
+            Console.WriteLine();
+
+            Console.WriteLine("Operation to complete:");
+            Console.WriteLine($"- Date: {operationToFill.Date}");
+            Console.WriteLine($"- Flow: {operationToFill.Flow}");
+            Console.WriteLine($"- Label: {operationToFill.Label}");
+            Console.WriteLine();
+
+            Console.Write("Enter a category number: ");
+            operationToFill.Category = consoleCategories[Console.ReadLine().Trim()];
+            Console.Write("Enter a comment: ");
+            operationToFill.Comment = Console.ReadLine().Trim().Replace(";","_");
+            Console.Clear();
+            bankDataBaseService.UpdateOperations([operationToFill]);
+        }
     }
 }

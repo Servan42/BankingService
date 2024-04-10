5ea0ff9482a3b0c1b811abaf2a45482312ecfd92
Servan42 <charlot.servan@gmail.com>
2024-04-10 23:48:45 +0200

    feat: Straight forward changes for config tables

---
+++ BankingService.Infra.Database/Model/CategoriesAndAutoComments.cs
@@ -0,0 +1,41 @@
+﻿using BankingService.Core.SPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingService.Infra.Database.Model
+{
+    internal class CategoriesAndAutoComments
+    {
+        public Dictionary<string, OperationCategoryAndAutoComment> Data { get; }
+        private CategoriesAndAutoComments(Dictionary<string, OperationCategoryAndAutoComment> data)
+        {
+            this.Data = data;
+        }
+
+        public static CategoriesAndAutoComments Load(IEnumerable<string> csvLines)
+        {
+            return new CategoriesAndAutoComments(
+                csvLines
+                .Select(l => l.Split(";"))
+                .ToDictionary(s => s[0], s => new OperationCategoryAndAutoComment(s[0], int.Parse(s[1]), s[2]))
+                );
+        }
+    }
+
+    internal class OperationCategoryAndAutoComment
+    {
+        public OperationCategoryAndAutoComment(string stringToScan, int categoryId, string autoComment)
+        {
+            StringToScan = stringToScan;
+            CategoryId = categoryId;
+            AutoComment = autoComment;
+        }
+
+        public string StringToScan { get; set; }
+        public int CategoryId { get; set; }
+        public string AutoComment { get; set; }
+    }
+}
---
+++ BankingService.Infra.Database/Model/PaypalCategories.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingService.Infra.Database.Model
+{
+    internal class PaypalCategories
+    {
+        public Dictionary<string, int> Data { get; }
+        private PaypalCategories(Dictionary<string, int> data)
+        {
+            this.Data = data;
+        }
+
+        public static PaypalCategories Load(IEnumerable<string> csvLines)
+        {
+            return new PaypalCategories(
+                csvLines
+                .Select(l => l.Split(";"))
+                .ToDictionary(s => s[0], s => int.Parse(s[1]))
+                );
+        }
+    }
+}

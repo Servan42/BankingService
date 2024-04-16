a322b0414f0f7ea9dcaf38b7da50cbf968867f7e
Servan42 <charlot.servan@gmail.com>
2024-04-17 01:15:48 +0200

    ref: Finsied user manual input in console and refactored it

---
+++ BankingService.ConsoleApp/EnhancedConsole.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingService.ConsoleApp
+{
+    internal static class EnhancedConsole
+    {
+        internal static void DisplayStringsOnXColumns(int columnNumbers, int leftPadding, List<string> strings)
+        {
+            var queue = new Queue<string>(strings);
+            var initialLine = Console.GetCursorPosition().Top;
+            var linesPerColumn = (int) Math.Ceiling(strings.Count / (decimal)columnNumbers);
+            
+            var currentLine = initialLine;
+            var colIndent = leftPadding;
+            var biggestStringLength = 0;
+
+            while(queue.Count > 0)
+            {
+                if(currentLine - initialLine >= linesPerColumn)
+                {
+                    currentLine = initialLine;
+                    colIndent += biggestStringLength + 4;
+                    biggestStringLength = 0;
+                }
+                var data = queue.Dequeue();
+                biggestStringLength = Math.Max(biggestStringLength, data.Length);
+                Console.SetCursorPosition(colIndent, currentLine);
+                Console.Write(data);
+                currentLine++;
+            }
+
+            Console.SetCursorPosition(0, initialLine + linesPerColumn + 1);
+        }
+
+        internal static void WriteWithForeGroundColor(string data, ConsoleColor color, bool newLine)
+        {
+            var backup = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(data);
+            Console.ForegroundColor = backup;
+            if(newLine) Console.WriteLine();
+        }
+    }
+}
---
+++ BankingService.ConsoleApp/UserInteractionManager.cs
@@ -0,0 +1,95 @@
+﻿using BankingService.Core.API.Interfaces;
+using BankingService.Core.SPI.DTOs;
+using BankingService.Core.SPI.Interfaces;
+
+namespace BankingService.ConsoleApp
+{
+    internal class UserInteractionManager
+    {
+        private readonly IImportService importService;
+        private readonly IBankDatabaseService bankDataBaseService;
+
+        public UserInteractionManager(IImportService importService, IBankDatabaseService bankDataBaseService)
+        {
+            this.importService = importService;
+            this.bankDataBaseService = bankDataBaseService;
+        }
+
+        internal void ExecuteManualFillLoop()
+        {
+            Dictionary<string, string> consoleCategories = LoadCategoriesWithIndexes();
+
+            var operationsToFill = bankDataBaseService.GetOperationsThatNeedsManualInput();
+            int operationCount = 0;
+            foreach (var operationToFill in operationsToFill)
+            {
+                Console.WriteLine($"--- Filling operation {++operationCount}/{operationsToFill.Count} ---\n");
+                Console.WriteLine("Categories:\n");
+                EnhancedConsole.DisplayStringsOnXColumns(3, 2, consoleCategories.Select(cat => $"[{cat.Key,2}]: {cat.Value}").ToList());
+                
+                DisplayOperationToFill(operationToFill);
+                var (category, comment) = PromptCategoryAndComment(consoleCategories);
+
+                var filledOperation = operationToFill with
+                {
+                    Category = category,
+                    Comment = comment
+                };
+
+                bankDataBaseService.UpdateOperations([filledOperation]);
+                Console.Clear();
+            }
+        }
+
+        private (string category, string comment) PromptCategoryAndComment(Dictionary<string, string> consoleCategories)
+        {
+            string category;
+            string comment;
+            do
+            {
+                Console.SetCursorPosition(0, Console.GetCursorPosition().Top);
+                Console.Write("Enter a category number: ");
+                category = consoleCategories[Console.ReadLine().Trim()];
+                Console.Write("Enter a comment: ");
+                comment = Console.ReadLine().Trim().Replace(";", "_");
+                
+                Console.WriteLine("\nAbout to update with:\n");
+                Console.Write("  - Category: ");
+                EnhancedConsole.WriteWithForeGroundColor(category, ConsoleColor.Green, true);
+                Console.Write("  - Comment: ");
+                EnhancedConsole.WriteWithForeGroundColor(comment, ConsoleColor.Green, true);
+                Console.WriteLine("\nPress ENTER to save, press anything else to retry...");
+            } 
+            while (Console.ReadKey().Key != ConsoleKey.Enter);
+            
+            return (category, comment);
+        }
+
+        private void DisplayOperationToFill(OperationDto operationToFill)
+        {
+            Console.WriteLine("Operation to complete:\n");
+            Console.Write("  - Date: ");
+            EnhancedConsole.WriteWithForeGroundColor(operationToFill.Date.ToString("d"), ConsoleColor.Cyan, true);
+            Console.Write("  - Flow: ");
+            EnhancedConsole.WriteWithForeGroundColor(operationToFill.Flow.ToString(), ConsoleColor.Cyan, true);
+            Console.Write("  - Label: ");
+            EnhancedConsole.WriteWithForeGroundColor(operationToFill.Label, ConsoleColor.Cyan, true);
+            Console.Write("  - Type: ");
+            EnhancedConsole.WriteWithForeGroundColor(operationToFill.Type, ConsoleColor.Cyan, true);
+            Console.WriteLine();
+        }
+
+        private Dictionary<string, string> LoadCategoriesWithIndexes()
+        {
+            var consoleCategories = new Dictionary<string, string>();
+            int i = 1;
+            foreach (var cat in bankDataBaseService.GetAllCategoriesNames())
+            {
+                if (cat == "TODO") continue;
+                consoleCategories.Add(i.ToString(), cat);
+                i++;
+            }
+            return consoleCategories;
+        }
+    }
+}

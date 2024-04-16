0e28237b71ce7b9e3ef9b1bf8fdefcd74ced7702
Servan42 <charlot.servan@gmail.com>
2024-04-17 01:15:48 +0200

    ref: Finsied user manual input in console and refactored it

---
+++ BankingService.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using BankingService.ConsoleApp;
+using BankingService.Core.API.Interfaces;
+using BankingService.Core.Services;
+using BankingService.Core.SPI.Interfaces;
+using BankingService.Infra.Database.Services;
+using BankingService.Infra.FileSystem.Adapters;
+using BankingService.Infra.FileSystem.Services;
+using NLog;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Hello, World!");
+
+            var fileSystemService = new FileSystemService();
+            var fileSystemServiceCore = new FileSystemAdapterCore(fileSystemService);
+            var fileSystemServiceDatabase = new FileSystemAdapterDatabase(fileSystemService);
+            IBankDatabaseService bankDataBaseService = new BankDatabaseService(fileSystemServiceDatabase);
+            IImportService importService = new ImportService(fileSystemServiceCore, bankDataBaseService);
+
+            bankDataBaseService.BackupDatabase();
+            importService.RecomputeEveryOperationAdditionalData();
+            importService.ImportBankFile(@"F:\Servan\Autres\Code\C#\BankCSVParser\publish\CSV\TODO\00021505101_Jan.csv");
+            importService.ImportPaypalFile(@"F:\Servan\Autres\Code\C#\BankCSVParser\publish\CSV\TODO\paypa_janvier.CSV");
+
+            var uiManager = new UserInteractionManager(importService, bankDataBaseService);
+            uiManager.ExecuteManualFillLoop();
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ResetColor();
+            LogManager.GetCurrentClassLogger().Fatal(ex);
+        }
+    }
+}

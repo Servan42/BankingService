5edf0edfa062b709ee021775cf53b81cb70ba1bc
Servan42 <charlot.servan@gmail.com>
2024-05-03 23:02:50 +0200

    feat(frontend): Added header and the two main componenets

--- BankingService.Api/ClientApp/src/app/app.component.ts
+++ BankingService.Api/ClientApp/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TransactionsComponent,
+    DashboardComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ClientApp';
+  selectedScreen: number = 1;
 }

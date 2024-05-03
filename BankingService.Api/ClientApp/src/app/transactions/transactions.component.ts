cb8e00761d3d6afbb445b968d8ad53cbd02c68a7
Servan42 <charlot.servan@gmail.com>
2024-05-04 00:39:45 +0200

    wip: filling transaction screen

--- BankingService.Api/ClientApp/src/app/transactions/transactions.component.ts
+++ BankingService.Api/ClientApp/src/app/transactions/transactions.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MAT_DATE_LOCALE, provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.css'],
   standalone: true,
+  providers: [provideNativeDateAdapter(), {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'}],
+  imports: [
+    CommonModule,
+    MatFormFieldModule, 
+    MatDatepickerModule
+  ]
 })
 export class TransactionsComponent implements OnInit {
 

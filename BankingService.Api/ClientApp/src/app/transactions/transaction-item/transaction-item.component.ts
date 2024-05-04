8b6a957c4772e835d7c5799cc7579501ecf06dea
Servan42 <charlot.servan@gmail.com>
2024-05-04 13:42:09 +0200

    feat: wip on the edit button on the transaction componenet

--- BankingService.Api/ClientApp/src/app/transactions/transaction-item/transaction-item.component.ts
+++ BankingService.Api/ClientApp/src/app/transactions/transaction-item/transaction-item.component.ts
@@ -1,19 +1,37 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Transaction } from '../../model/transaction';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-item',
   templateUrl: './transaction-item.component.html',
   styleUrls: ['./transaction-item.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule
+  ]
 })
 export class TransactionItemComponent implements OnInit {
 
   @Input() transaction!: Transaction;
+  isEditableCommentDisabled: boolean = true;
+  editButtonText: string = 'Edit';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onEditTransactionClicked(): void{
+    if(this.isEditableCommentDisabled === true) {
+      this.editButtonText = 'Save';
+      // TODO save to db
+    } else {
+      this.editButtonText = 'Edit';
+    }
+    this.isEditableCommentDisabled = !this.isEditableCommentDisabled;
   }
 
 }

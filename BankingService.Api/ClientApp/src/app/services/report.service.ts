afeb8cf596471e6e4a1073e867051aa2ec893284
Servan42 <charlot.servan@gmail.com>
2024-07-19 00:58:04 +0200

    wip: added error handler on getReport in frontend to poc it

--- BankingService.Api/ClientApp/src/app/services/report.service.ts
+++ BankingService.Api/ClientApp/src/app/services/report.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable, map, of } from 'rxjs';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
 import { Transaction } from '../model/transaction';
 import { TransactionReport, mockReport } from '../model/transaction-report';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ReportInput } from '../model/report-input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ErrorSnackbarComponent } from '../sncackbars/error-snackbar/error-snackbar.component';
 
 const ENDPOINT = environment.apiUrl + '/api/Report/';
 
@@ -12,7 +14,7 @@ const ENDPOINT = environment.apiUrl + '/api/Report/';
   providedIn: 'root',
 })
 export class ReportService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private snackBar: MatSnackBar) {}
 
   getReport(reportInput: ReportInput): Observable<TransactionReport> {
     console.log('DB CALLED! (getReport)');
@@ -26,8 +28,19 @@ export class ReportService {
           endDate: new Date(r.endDate),
           highestTransactions: r.highestTransactions.map((t) => ({...t, date: new Date(t.date)})),
           treasuryGraphData: r.treasuryGraphData.map((data) => ({...data, dateTime: new Date(data.dateTime)}))
-        }))
+        })),
+        catchError(err => this.handleError(err))
       );
     // return of(mockReport);
+  }
+
+  private handleError(error: HttpErrorResponse) : Observable<never> {
+    if (error.status === 0) {
+      console.error('A client-side or network error occurred:', error.error);
+      this.snackBar.open('A client-side or network error occurred.', '✔', { duration: 10000 })
+    } else {
+      this.snackBar.openFromComponent(ErrorSnackbarComponent, { data: error })
+    }
+    return throwError(() => new Error('An HTTP error occured.'));
   }
 }

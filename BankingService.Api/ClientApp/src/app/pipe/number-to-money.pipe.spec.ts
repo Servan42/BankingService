4636f3ed54e7f63713517b2786204d7ba0a6b4f9
Servan42 <charlot.servan@gmail.com>
2024-08-07 02:17:20 +0200

    ref: delete all unused spec files

--- BankingService.Api/ClientApp/src/app/pipe/number-to-money.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumberToMoneyPipe } from './number-to-money.pipe';
-
-describe('NumberToMoneyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NumberToMoneyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
